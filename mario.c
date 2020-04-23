5925b9f98405105e18a98932e7cc4c74aafa4d9c
banseedhar786 <banseedhar786@users.noreply.github.com>
2020-04-23 06:36:56 +0000

    automated commit by cevsubmit [lang=C.UTF-8]

---
+++ mario.c
@@ -0,0 +1,26 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{  int n;
+   
+   do
+   { 
+     n=get_int("height :\n");
+
+   }while(n<1||n>8);
+
+   int i,j;
+   for(i=1;i<=n;i++)
+   { 
+       for(j=1;j<=n-i;j++)
+       printf(" ");
+       for(j=1;j<=i;j++)
+       printf("#");
+       printf("  ");
+       for(j=1;j<=i;j++)
+       printf("#");
+       printf("\n");
+     }
+
+}
